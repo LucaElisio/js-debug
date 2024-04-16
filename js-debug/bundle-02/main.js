8bcfdac1aaa67966a5dc33783a8c1e7acf88817f
LucaElisio <personal.elix@gmail.com>
2024-04-16 15:48:14 +0200

    feat: bundle-2

--- js-debug/bundle-02/main.js
+++ js-debug/bundle-02/main.js
@@ -22,11 +22,40 @@ function checkAge() {
     }
 }
 checkAge();
+// 1- Questa funzione restituisce una frase in base all'età
+// 2- la variabile message deve essere dichiarata tramite let in quanto cambia valore
+// 3- manca il return della variabile message alla fine della funzione
+
+//* ESERCIZIO 1 CORRETTO
+function checkAge() {
+    const myAge = 32;
+    let message = '';
+
+    if (myAge < 18) {
+        message = `Sei troppo giovane! Hai ${myAge} anni!`;
+    } else {
+        message = 'Hai più di 18 anni!';
+    }
+
+    return message;
+}
+checkAge();
+
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
     console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+}
+printColorsNumber();
+// 1- Questa funzione stampa la quantità di elementi presenti in un array
+// 2- length è scritto male
+// 3- Non ci sono errori logici
+
+//* ESERCIZIO 2 CORRETTO
+function printColorsNumber() {
+    const colors = ['blue', 'red', 'yellow', 'green', 'black'];
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`);
 }
 printColorsNumber();
 
@@ -39,6 +68,19 @@ function addNumbers() {
     console.log(`Il risultato finale è ${total}`);
 }
 addNumbers();
+// 1- Questa funzione restituisce una somma dato un numero in input
+// 2- Non ci sono errori di sintassi
+// 3- userNumber è stringa, va trasformata in numero, altrimenti fa la concatenazione
+
+//* ESERCIZIO 3 CORRETTO
+function addNumbers() {
+    const userNumber = parseInt(prompt('Inserisci un numero'));
+    const total = userNumber + 12;
+
+    console.log(`Il risultato finale è ${total}`);
+}
+addNumbers();
+
 
 
 // ESERCIZIO 4
@@ -59,6 +101,28 @@ function checkAccess() {
     }
 }
 checkAccess();
+// 1- Questa funzione restituisce una stringa in base alla presenza o meno di un valore dentro l'array
+// 2- false e true vanno scritti senza apici
+
+//* ESERCIZIO 4 CORRETTO
+function checkAccess() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    if (addresses.includes(userEmail)) {
+        grantAccess = true;
+    }
+
+    if (grantAccess === true) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+}
+checkAccess();
+
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -76,7 +140,6 @@ function checkAccessImproved() {
 
             if (email === userEmail) {
                 grantAccess = 'true';
-
             }
 
         }
@@ -88,6 +151,41 @@ function checkAccessImproved() {
         }
     }
     checkAccessImproved();
+
+// 1- Questa funzione restituisce una stringa in base alla presenza o meno di un valore dentro un array e solo se la lunghezza di questo valore è maggiore di 5
+// 2- suggerimento sbagliato, false e true andrebbero fuori apici, la funzione andrebbe richiamata fuori dalla stessa, manca la parentesi graffa alla fine della funzione
+// 3- if finale deve stare fuori dal ciclo for
+
+//* ESERCIZIO 5 CORRETTO
+function checkAccessImproved() {
+    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+
+    const userEmail = prompt('Inserisci il tuo indirizzo email');
+
+    let grantAccess = false;
+
+    for (let i = 0; i < addresses.length; i++) {
+        const email = addresses[i];
+
+        if (userEmail.length > 5) {
+
+            if (email === userEmail) {
+                grantAccess = true;
+
+            }
+
+        }
+    }
+
+    if (grantAccess) {
+        console.log('Accesso consentito!');
+    } else {
+        console.log('Accesso negato!');
+    }
+    
+};
+checkAccessImproved();
+
 
 
 
