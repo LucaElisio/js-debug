0cf0cf54006bdc944a603e287791403c31015d03
LucaElisio <personal.elix@gmail.com>
2024-04-16 16:08:37 +0200

    feat: debug-objects

--- js-debug-objects/main.js
+++ js-debug-objects/main.js
@@ -84,3 +84,83 @@ console.log(dieselCars);
 
 console.log('Tutte le altre auto');
 console.log(otherCars);
+
+1- Questo codice stamperà in console tre array di oggetti, uno con gli oggetti che hanno come type "benzina", uno con gli oggetti che hanno come type "diesel", uno con il resto.
+2- Manca una virgola a riga 59 dopo l'oggetto, riga 67 al posto di >= va =>, nella creazione di dieselCars e otherCars non vanno aggiunte le graffe, nella condizione di otherCars al posto di OR va AND e va tolto return.
+3- bisogna inserire toLowerCase dopo auto.type perchè alcuni valori sono scritti con la lettera maiuscola
+
+
+//* ESERCIZIO CORRETTO
+
+const cars = [
+    {
+        manufacturer: 'Ford',
+        model: 'Fiesta',
+        type: 'diesel'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'A1',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Golf',
+        type: 'Benzina'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Panda',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Fiat',
+        model: 'Multipla',
+        type: 'GPL'
+    },
+    {
+        manufacturer: 'Tesla',
+        model: 'Model 3',
+        type: 'elettrico'
+    },
+    {
+        manufacturer: 'Volkswagen',
+        model: 'Polo',
+        type: 'benzina'
+    },
+    {
+        manufacturer: 'Ford',
+        model: 'Kuga',
+        type: 'Diesel'
+    },
+    {
+        manufacturer: 'Seat',
+        model: 'Ibiza',
+        type: 'metano'
+    },
+    {
+        manufacturer: 'Audi',
+        model: 'R8',
+        type: 'Benzina'
+    },
+];
+
+const gasolineCars = cars.filter( (auto) => auto.type.toLowerCase() === 'benzina');
+// console.log(gasolineCars);
+
+const dieselCars = cars.filter( (auto) => auto.type.toLowerCase() === 'diesel');
+// console.log(dieselCars);
+
+const otherCars = cars.filter((auto) => auto.type.toLowerCase() !== 'benzina' && auto.type.toLowerCase() !== 'diesel');
+// console.log(otherCars);
+
+console.log('Auto a benzina');
+console.log('*******************************');
+console.log(gasolineCars);
+
+console.log('Auto a diesel');
+console.log('*******************************');
+console.log(dieselCars);
+
+console.log('Tutte le altre auto');
+console.log(otherCars);
